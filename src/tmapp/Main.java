6d53844297048b26a5ea8b5d8db122d0dd5f1866
Florian Michalzik <florian.michalzik@adesso-insurance-solutions.de>
2023-08-27 22:05:54 +0200

    Added Team class and testcode in main

--- src/tmapp/Main.java
+++ src/tmapp/Main.java
@@ -1,15 +1,26 @@
 package tmapp;
 
 import tmapp.player.FemalePlayer;
+import tmapp.player.MalePlayer;
 import tmapp.player.Player;
+import tmapp.team.Team;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         Player player1 = new FemalePlayer(1, "Maxine Mustermann", 27, 1);
+        Player player2 = new MalePlayer(2, "Max Mustermann", 35, 2);
         player1.playerInfo();
 
+        Team team = new Team("Geiles Team", new ArrayList<>());
+        team.addPlayer(player2);
+
+        System.out.println(team.getTeamName());
+        team.displayPlayers();
+
     }
+
 }
---
+++ src/tmapp/team/Team.java
@@ -0,0 +1,41 @@
+package tmapp.team;
+
+import tmapp.player.FemalePlayer;
+import tmapp.player.MalePlayer;
+import tmapp.player.Player;
+
+import java.util.ArrayList;
+
+public class Team {
+
+    private String teamName;
+
+    private ArrayList<Player> players;
+
+    public Team(String teamName, ArrayList<Player> players) {
+        this.teamName = teamName;
+        this.players = players;
+    }
+
+/*    public void addFemalePlayer(int playerID, String playerName, int playerAge, int playerRank) {
+        players.add(new FemalePlayer(playerID, playerName, playerAge, playerRank));
+    }*/
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void displayPlayers(){
+        for (Player player : players) {
+            player.playerInfo();
+        }
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+}
