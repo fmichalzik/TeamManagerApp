b85c92f8b99e419d7fe67d0d74f118bac51b4327
Florian Michalzik <fmichalzik@web.de>
2023-08-30 22:11:12 +0200

    Commenting and added methods to remove all female/male players from team (players list)

--- src/tmapp/Main.java
+++ src/tmapp/Main.java
@@ -14,16 +14,18 @@ public class Main {
         Player player1 = new FemalePlayer(1, "Maxine Mustermann", 27, 1);
         Player player2 = new MalePlayer(2, "Max Mustermann", 35, 2);
         Player player3 = new MalePlayer(3, "Klaus Staplerfahrer", 55, 55);
-        player1.playerInfo();
 
         Team team = new Team("Geiles Team", new ArrayList<>());
+        team.addPlayer(player1);
         team.addPlayer(player2);
         team.addPlayer(player3);
 
         System.out.println(team.getTeamName());
         team.displayPlayers();
 
-        team.removePlayerByPlayerID(2);
+        System.out.println("Remove all male Players");
+        team.removeAllMalePlayers();
+        //team.removePlayerByPlayerID(2);
         team.displayPlayers();
 
     }
--- src/tmapp/team/Team.java
+++ src/tmapp/team/Team.java
@@ -10,8 +10,10 @@ public class Team {
 
     private String teamName;
 
+    // Creates a list to hold player objects
     private ArrayList<Player> players;
 
+    // Constructor to instanciate a team with a team name and a list of players
     public Team(String teamName, ArrayList<Player> players) {
         this.teamName = teamName;
         this.players = players;
@@ -21,14 +23,18 @@ public class Team {
         players.add(new FemalePlayer(playerID, playerName, playerAge, playerRank));
     }*/
 
+    // Adds a player (femalePlayer or malePlayer subtype) to the list of players
     public void addPlayer(Player player) {
         players.add(player);
     }
 
+    // Removes a player (femalePlayer or malePlayer subtype) from the list of players
+    // not very specific - removePlayerByAttribute might be better
     public void removePlayer(Player player) {
         players.remove(player);
     }
 
+    // Removes a player by playerID (femalePlayer or malePlayer subtype) from the list of players
     public void removePlayerByPlayerID(int playerID) {
         for (Player player : players) {
             int id = player.getPlayerID();
@@ -38,6 +44,32 @@ public class Team {
         }
     }
 
+    // Removes all male players from the list of players
+    // It "for each" goes through the players list and collects all MalePlayers in a new internal
+    // maleplayers list.
+    public void removeAllMalePlayers() {
+        ArrayList<Player> malePlayers = new ArrayList<>();
+        for (Player player : players) {
+            if (player instanceof MalePlayer) {
+                malePlayers.add(player);
+            }
+        }
+        // Remove all elements from players list present in malePlayers list
+        players.removeAll(malePlayers);
+    }
+    public void removeAllFemalePlayers() {
+        ArrayList<Player> femalePlayers = new ArrayList<>();
+        for (Player player : players) {
+            if (player instanceof FemalePlayer) {
+                femalePlayers.add(player);
+            }
+        }
+        // Remove all elements from players list present in malePlayers list
+        players.removeAll(femalePlayers);
+    }
+
+
+    // Shows playerInfo per player in the players list
     public void displayPlayers(){
         for (Player player : players) {
             player.playerInfo();
